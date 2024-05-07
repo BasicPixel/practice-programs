834b57c675483b6d44ddf4d55bb48ba457f9227c
Pixel <nightknight218@gmail.com>
2024-05-07 10:01:09 +0300

    Close input in sum

--- java/Sum.java
+++ java/Sum.java
@@ -4,17 +4,19 @@ public class Sum {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
+
 		int num1, num2, sum;
-		
+
 		System.out.println("Enter num1: ");
 		num1 = input.nextInt();
-		
+
 		System.out.println("Enter num2: ");
 		num2 = input.nextInt();
-		
+
 		sum = num1 + num2;
 		System.out.printf("Sum is %d\n", sum);
+
+		input.close();
 	}
 
 }
